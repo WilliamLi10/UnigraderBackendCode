ade856c56ac972fff1582ae7c3e221717543d42e
WilliamLi10 <william3ny@gmail.com>
2023-01-27 10:30:46 -0500

    Github Setup

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "net/http"
+    "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+    "fmt"
+)
+func main() {
+    r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w,r, "public/index.html")
+	})
+
+    r.Post("/submit", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println(r.FormValue("email"))
+    })
+
+    http.ListenAndServe(":8080", r)
+}
